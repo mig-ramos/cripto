60969a1126f09041e23f45d96632614ccb21dc24
Miguel Arcanjo <mig.ramos10@gmail.com>
2023-12-05 18:00:20 -0300

    feat: Acessa API, formata dois campos

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,8 +1,63 @@
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import styles from "./home.module.css";
 import { BiSearch } from "react-icons/bi";
 
+interface CoinProps {
+  name: string;
+  delta_24h: string;
+  price: string;
+  symbol: string;
+  volume_24h: string;
+  market_cap: string;
+  formatedPrice: string;
+  formatedMarket: string;
+}
+
+interface DataProps {
+  coins: CoinProps[];
+}
+
 export function Home() {
+  const [coins, setCoins] = useState<CoinProps[]>([]);
+
+  useEffect(() => {
+    async function getData(){
+      fetch('https://sujeitoprogramador.com/api-cripto/?key=8dc2b09b06d97719&pref=BRL&page=1&order=volume_desc')
+      
+      .then(response => response.json())
+      .then((data: DataProps) => {
+        // arequisicao deu tudo certo
+        // console.log(data);
+        const coinsData = data.coins.slice(0,15)
+        // console.log(coinsData);
+
+        const price = Intl.NumberFormat("pt-BR",{
+          style: "currency",
+          currency: "BRL"
+        })
+        const formatResult = coinsData.map((item) => {
+          const formated = {
+            ...item,
+            formatedPrice: price.format(Number(item.price)),
+            formatedMarket: price.format(Number(item.market_cap)),
+          }
+
+          return formated;
+        })
+
+        // console.log(formatResult);
+        setCoins(formatResult);
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+    }
+
+    getData();
+
+  },[])
+
   return (
     <main className={styles.container}>
       <form className={styles.form} action="">
