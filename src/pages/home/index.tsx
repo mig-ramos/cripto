596e38bb372f3b846ac391a2ed5225eb111577ea
Miguel Arcanjo <mig.ramos10@gmail.com>
2023-12-05 20:13:17 -0300

    feat: Renderiza a lista

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -22,41 +22,41 @@ export function Home() {
   const [coins, setCoins] = useState<CoinProps[]>([]);
 
   useEffect(() => {
-    async function getData(){
-      fetch('https://sujeitoprogramador.com/api-cripto/?key=8dc2b09b06d97719&pref=BRL&page=1&order=volume_desc')
-      
-      .then(response => response.json())
-      .then((data: DataProps) => {
-        // arequisicao deu tudo certo
-        // console.log(data);
-        const coinsData = data.coins.slice(0,15)
-        // console.log(coinsData);
+    async function getData() {
+      fetch(
+        "https://sujeitoprogramador.com/api-cripto/?key=8dc2b09b06d97719&pref=BRL"
+      )
+        .then(response => response.json())
+        .then((data: DataProps) => {
+          // arequisicao deu tudo certo
+          // console.log(data);
+          const coinsData = data.coins.slice(0, 15);
+          // console.log(coinsData);
 
-        const price = Intl.NumberFormat("pt-BR",{
-          style: "currency",
-          currency: "BRL"
+          const price = Intl.NumberFormat("pt-BR", {
+            style: "currency",
+            currency: "BRL",
+          });
+          const formatResult = coinsData.map((item) => {
+            const formated = {
+              ...item,
+              formatedPrice: price.format(Number(item.price)),
+              formatedMarket: price.format(Number(item.market_cap)),
+            };
+
+            return formated;
+          });
+
+          // console.log(formatResult);
+          setCoins(formatResult);
         })
-        const formatResult = coinsData.map((item) => {
-          const formated = {
-            ...item,
-            formatedPrice: price.format(Number(item.price)),
-            formatedMarket: price.format(Number(item.market_cap)),
-          }
-
-          return formated;
-        })
-
-        // console.log(formatResult);
-        setCoins(formatResult);
-      })
-      .catch((err) => {
-        console.log(err);
-      })
+        .catch((err) => {
+          console.log(err);
+        });
     }
 
     getData();
-
-  },[])
+  }, []);
 
   return (
     <main className={styles.container}>
@@ -77,16 +77,24 @@ export function Home() {
           </tr>
         </thead>
         <tbody id="tbody">
-          <tr className={styles.tr}>
+         {coins.map(coin => (
+            <tr key={coin.name} className={styles.tr}>
             <td className={styles.tdLabel} data-label="Moeda">
-              <Link className={styles.link} to={"/detail/btc"}>
-                <span>Bitcoin</span> | BTC
+              <Link className={styles.link} to={`/detail/${coin.symbol}`}>
+                <span>{coin.name}</span> | {coin.symbol}
               </Link>
             </td>
-            <td className={styles.tdLabel} data-label="Mercado">R$ 19293</td>
-            <td className={styles.tdLabel} data-label="Preço">R$ 40.96233</td>
-            <td className={styles.tdLoss} data-label="Volume"><span>-5.3</span></td>
+            <td className={styles.tdLabel} data-label="Mercado">
+              {coin.formatedMarket}
+            </td>
+            <td className={styles.tdLabel} data-label="Preço">
+              {coin.formatedPrice}
+            </td>
+            <td className={Number(coin?.delta_24h.replace(',','.')) >= 0 ? styles.tdProfit : styles.tdLoss} data-label="Volume">
+              <span>{coin.delta_24h}</span>
+            </td>
           </tr>
+          ))}
         </tbody>
       </table>
     </main>
