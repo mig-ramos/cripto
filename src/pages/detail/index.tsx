c59f80c55b505dc2c7cf962e691647ead619a587
Miguel Arcanjo <mig.ramos10@gmail.com>
2023-12-05 23:34:16 -0300

    feat: Detalhe da cripto moeda

--- src/pages/detail/index.tsx
+++ src/pages/detail/index.tsx
@@ -1,11 +1,68 @@
+import { useEffect, useState } from 'react';
 import styles from './detail.module.css';
 import { useParams } from 'react-router-dom';
 
+interface CoinProp{
+    symbol: string;
+    name: string;
+    price: string;
+    market_cap: string;
+    low_24h: string;
+    high_24h: string;
+    total_volume_24h: string;
+    delta_24h: string;
+    formatedPrice: string;
+    formatedMarket: string;
+    formatedLowprice: string;
+    formatedHighprice: string;
+    error?: string;
+}
+
 export function Datail(){
     const { cripto } =useParams();
+    const [detail, setDetail] = useState<CoinProp>();
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        function getData(){
+            fetch(`https://sujeitoprogramador.com/api-cripto/coin/?key=8dc2b09b06d97719&pref=BRL&symbol=${cripto}`)
+            .then(response => response.json())
+            .then((data: CoinProp) => {
+                // console.log(data)
+                const price = Intl.NumberFormat("pt-BR", {
+                    style: "currency",
+                    currency: "BRL",
+                  });
+
+                  const resultData = {
+                    ...data,
+                    formatedPrice: price.format(Number(data.price)),
+                    formatedMarket: price.format(Number(data.market_cap)),
+                    formatedLowprice: price.format(Number(data.low_24h)),
+                    formatedHighprice: price.format(Number(data.high_24h)),
+                  }
+
+                  setDetail(resultData);
+                  setLoading(false);
+                //   console.log(resultData)
+            })
+        }
+
+        getData();
+    }, [cripto])
+
+    if(loading){
+        return(
+           <div className={styles.container}>
+            <h4 className={styles.center}>Carregando informações...</h4>
+           </div> 
+        )
+    }
+
     return(
-        <div>
-            <h1>Página Detalhes {cripto}</h1>
+        <div className={styles.container}>
+            <h1 className={styles.center}>{detail?.name}</h1>
+            <p className={styles.center}>{detail?.symbol}</p>
         </div>
     )
 }
